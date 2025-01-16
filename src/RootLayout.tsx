39d49a80f29b3389b87a5b2b7caa13282ce557d1
Minyeong Jeong <megormin@gmail.com>
2025-01-16 14:12:32 +0100

    header added

--- src/RootLayout.tsx
+++ src/RootLayout.tsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const RootLayout = () => {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>{" "}
+      <Footer />
+    </>
   );
 };
 
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,9 +1,6 @@
 import { Tables } from "./supabase";
 
 export type Recipe = Tables<"recipes">;
-export type Categories = Tables<"categories">;
-export type Recipes_ingredients = Tables<"recipes_ingredients">;
-export type Ingredients = Tables<"ingredients">;
 
 export type JoinedRecipe = Tables<"recipes"> & {
   categories: Pick<Tables<"categories">, "name">;
