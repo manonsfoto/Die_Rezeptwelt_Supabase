d47d10361bb4726d56cddf9f615f52641481a16a
Minyeong Jeong <megormin@gmail.com>
2025-01-22 17:12:16 +0100

    RLS policy corrected

--- src/pages/MeineRezepte.tsx
+++ src/pages/MeineRezepte.tsx
@@ -3,6 +3,7 @@ import { supabase } from "../utils/supabaseClient";
 import { Recipe } from "../utils/types";
 import { UserContext } from "../context/Context";
 import NewCard from "../components/card/NewCard";
+import { Link } from "react-router-dom";
 
 const MeineRezepte = () => {
   const [favoriteRecipes, setFavoriteRecipes] = useState<Recipe[]>([]);
@@ -12,8 +13,7 @@ const MeineRezepte = () => {
       if (user) {
         const { data, error } = await supabase
           .from("recipe_favorites")
-          .select(`*,  recipes(*)`)
-          .eq("user_id", user.id);
+          .select(`*,  recipes(*)`);
 
         if (error) {
           console.error("Fehler beim Laden der Rezepte:", error.message);
@@ -32,29 +32,35 @@ const MeineRezepte = () => {
 
   return (
     <>
-      <section className="flex flex-col justify-center items-center mb-16">
-        <h1 className="font-bold text-3xl	my-12 ">Meine Rezepte 🥰</h1>
-        {favoriteRecipes.length > 0 ? (
-          <ul className="flex flex-col gap-4 flex-wrap">
-            {favoriteRecipes.map((recipe) => (
-              <li key={recipe.id}>
-                <NewCard recipe={recipe} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <div className="flex flex-col justify-center items-center gap-4 mt-8">
-            {" "}
-            <p className="text-2xl font-bold text-gray-600">
-              No favorite Recipes found
-            </p>
-            <p className="texl-xl text-gray-400">
-              When you click a 🩶 button on detailed recipes, it will appear
-              here.
-            </p>
-          </div>
-        )}
-      </section>
+      {user ? (
+        <section className="flex flex-col justify-center items-center mb-16">
+          <h1 className="font-bold text-3xl	my-12 ">Meine Rezepte 🥰</h1>
+          {favoriteRecipes.length > 0 ? (
+            <ul className="flex flex-col gap-4 flex-wrap">
+              {favoriteRecipes.map((recipe) => (
+                <li key={recipe.id}>
+                  <NewCard recipe={recipe} />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="flex flex-col justify-center items-center gap-4 mt-8">
+              {" "}
+              <p className="text-2xl font-bold text-gray-600">
+                No favorite Recipes found
+              </p>
+              <p className="texl-xl text-gray-400">
+                When you click a 🩶 button on detailed recipes, it will appear
+                here.
+              </p>
+            </div>
+          )}
+        </section>
+      ) : (
+        <Link to={"/login"} className="btn btn-secondary  mt-24 ">
+          Zum Log-In
+        </Link>
+      )}
     </>
   );
 };
