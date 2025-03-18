19cacceb9922fcbb399c997e48fa8c8cbbbd5941
Minyeong Jeong <megormin@gmail.com>
2025-03-18 14:03:39 +0100

    style: update my grocerylist

--- src/pages/MyGroceryList.tsx
+++ src/pages/MyGroceryList.tsx
@@ -6,6 +6,7 @@ import {
 } from "../context/Context";
 import { supabase } from "../utils/supabaseClient";
 import { GroceryList } from "../utils/types";
+import EmptyHero from "../components/EmptyHero";
 
 const MyGroceryList = () => {
   const { groceryList } = useContext(GroceryListContext);
@@ -74,46 +75,40 @@ const MyGroceryList = () => {
         <h1 className="text-3xl w-full my-12 pb-4 font-caprasimo border-b-2 border-black">
           Meine Einkaufsliste
         </h1>
-        <button
-          onClick={emptyGroceryList}
-          className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg  btn-warning w-fit self-end"
-        >
-          Liste leeren 🗑️
-        </button>
-        <div className="overflow-x-auto min-h-128">
-          <table className="table max-w-2xl">
-            <thead>
-              <tr className="text-base text-center">
-                <th></th>
-                <th>Artikelname</th>
-                <th>Menge</th>
-                <th>Einheit</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
+        {groceryList && groceryList.length > 0 ? (
+          <div className="bg-neutral rounded-3xl min-h-screen p-4 flex flex-col gap-4 items-center">
+            <button
+              onClick={emptyGroceryList}
+              className="btn rounded-full  btn-warning w-fit self-end"
+            >
+              Liste leeren 🗑️
+            </button>
+
+            <ul className=" bg-base-100  rounded-3xl pb-4">
               {groceryList?.map((item) => (
-                <tr key={item.ingredient_id}>
-                  <th>
-                    <label>
+                <li
+                  key={item.ingredient_id}
+                  className="flex items-center justify-between gap-4 md:gap-12 p-6 font-semibold  border-b-2 border-base-300  "
+                >
+                  <div className="relative">
+                    <label className="absolute -top-2 -left-2">
                       <input
                         type="checkbox"
                         name={`checkbox${item.ingredient_id}`}
-                        className="checkbox"
+                        className="checkbox w-5 h-5 bg-accent"
                         checked={item.completed}
                         onChange={(e: ChangeEvent<HTMLInputElement>) => {
                           changeCheckbox(item, e.target.checked);
                         }}
                       />
                     </label>
-                  </th>
-                  <td className="flex items-center justify-center">
+
                     <div className="font-bold font-gaegu text-xl italic bg-secondary rounded-2xl uppercase w-28 h-16 flex items-center justify-center">
                       {item.ingredients.name}
                     </div>
-                  </td>
-                  <td>
-                    <div className="flex items-center gap-3">
+                  </div>
+                  <div className="flex flex-col items-center justify-center gap-3 md:flex-row md:gap-10">
+                    <div className="flex items-center justify-center gap-3">
                       <button
                         onClick={() => {
                           handleQuantityChangeButton(item.ingredient_id);
@@ -134,37 +129,42 @@ const MyGroceryList = () => {
                         +
                       </button>
                     </div>
-                  </td>
-                  <td className="text-sm">{item.ingredients.unit}</td>
-                  <th>
-                    <button
-                      className="btn btn-square btn-sm"
-                      title="delete"
-                      onClick={() => {
-                        handleDeleteGroceryItem(item.ingredient_id);
-                      }}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-4 w-4"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
+                    <div className="flex items-center gap-3 md:gap-10">
+                      <p className="text-sm w-12 md:text-right">
+                        {item.ingredients.unit}
+                      </p>
+
+                      <button
+                        className="btn btn-square border-none btn-xs bg-transparent "
+                        title="delete"
+                        onClick={() => {
+                          handleDeleteGroceryItem(item.ingredient_id);
+                        }}
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
-                  </th>
-                </tr>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-4 w-4"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </li>
               ))}
-            </tbody>
-          </table>
-        </div>
+            </ul>
+          </div>
+        ) : (
+          <EmptyHero mainText="Keine Einkaufsliste" />
+        )}
       </section>
     </>
   );
